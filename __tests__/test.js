2e0663ef80016cae78954677aedaf0bcc0e97492
Pawel Makarewicz <pawelmakarewicz.1996@gmail.com>
2022-10-19 22:58:44 +0200

    add tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,21 +1,17 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { readFileSync } from 'node:fs';
 import * as path from 'node:path';
-import {jest} from '@jest/globals';
+import findDiff from '../lib/findDiffTwoJSONFiles.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-console.log(readFile('test1.json'));
-const obj = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
-
-test('gt', () => {
-  expect((obj)).toEqual(obj);
+const file1 = getFixturePath('test1.json');
+const file2 = getFixturePath('test2.json');
+test('findDiff', () => {
+  expect((findDiff(file1, file2))).toMatch('- follow: false');
+  expect((findDiff(file1, file2))).toMatch('host: hexlet.io');
+  expect((findDiff(file1, file2))).toMatch('- timeout: 50');
+  expect((findDiff(file1, file2))).toMatch('+ timeout: 20');
+  expect((findDiff(file1, file2))).toMatch('+ verbose: true');
 });
