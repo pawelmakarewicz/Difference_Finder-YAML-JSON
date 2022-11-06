e0c28fad8f79f08272fedc5e42e2509ce230d01e
Pawel Makarewicz <pawelmakarewicz.1996@gmail.com>
2022-11-06 23:57:33 +0100

    replace sort

--- src/parsers.js
+++ src/parsers.js
@@ -5,13 +5,10 @@ import * as path from 'node:path';
 const parse = (filepath) => {
   const data = readFileSync(filepath, 'utf8');
   const format = path.extname(filepath);
-  let parserFunction;
   if (format === 'js') {
-    parserFunction = JSON.parse;
-  } else {
-    parserFunction = yaml.load;
+    return JSON.parse(data);
   }
-  return parserFunction(data);
+  return yaml.load(data);
 };
 
 export default parse;
